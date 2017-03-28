dd50fe07fb4c974df9ba63c1be7232b6ab5ebea4
Sam Corcos <sam@corcos.io>
2017-03-27 19:08:04 -0700

    minimum reproducible bug

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const styles = {
+  outer: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    margin: 50,
+    height: 200,
+    width: 200,
+    border: '1px solid black',
+  },
+  inner: {
+    position: 'absolute',
+    top: 100,
+    left: 0,
+    width: 160,
+    marginLeft: 20,
+    borderTop: 'none',
+    borderLeft: 'none',
+    borderRight: 'none',
+    borderBottom: '1px solid black',
+    borderRadius: 0,
+    marginRight: 30,
+    fontSize: '2.6em',
+    textAlign: 'center',
+    backgroundColor: 'transparent',
+    outline: 'none',
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div style={styles.outer}>
+          <input style={styles.inner} />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
